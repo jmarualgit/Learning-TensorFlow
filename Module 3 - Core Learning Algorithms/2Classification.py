e563b8f2056c502ef2f06cd7442e855699156c2a
jmarualgit <jairelmarual@gmail.com>
2023-09-22 00:03:54 -0700

    Made dataframes and started input functions + more.
    I popped 'Species' column from train and test variables and put it into train_y and test_y respectively.

--- Module 3 - Core Learning Algorithms/2Classification.py
+++ Module 3 - Core Learning Algorithms/2Classification.py
@@ -24,3 +24,24 @@ train_path = tf.keras.utils.get_file(
     "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
 test_path = tf.keras.utils.get_file(
     "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+# train and test are pandas dataframes
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header = 0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header = 0)
+
+# pop to species column off and use it as label
+# this will remove the Species column from train and test and 
+# put it into variables train_y and test_y respectively
+train_y = train.pop('Species')
+test_y = test.pop('Species')
+
+# input function
+def input_fn(features, labels, training = true, batch_size = 256):
+    # convert the inputs to a dataset
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    
+    # shuffle and repeat if you are in training mode
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+    
+    return dataset.batch(batch_size)
